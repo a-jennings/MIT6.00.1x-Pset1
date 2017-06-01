9f70224d2c021870a2fa6fcfdff2d48d915a4b50
Andrew Jennings <andrew_jennings@ymail.com>
2017-06-01 17:32:38 +0100

    prob2 has index error

--- prob2.py
+++ prob2.py
@@ -3,3 +3,16 @@
 #For example, if s = 'azcbobobegghakl', then your program should print:
 #Number of times bob occurs is: 2
 
+s = 'bobobfpbobobbobboboobobobobbbbobb'
+total = 0
+
+for i in range(len(s)):
+    char = s[i]
+    if char == 'b':
+        if s[i]+s[i+1]+s[i+2] == 'bob':
+            total += 1
+        elif IndexError:
+            break
+print(total)
+        
+
